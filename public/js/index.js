a2e597a4d5b25fa4dba1718fe567bf2466e40479
Prince Lee <prince_lee.nna85@ymail.com>
2022-10-10 22:52:17 +0100

    Add Registration page

--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,17 @@
-alert('hello');
+// alert('hello');
+
+// Registration form password matched verification
+const checkMatchedPassword = function(){
+    if(document.getElementById("password").value == document.getElementById("confirm_password").value){
+        document.getElementById("message").style.color = "green";
+        document.getElementById("message").innerHTML = "matched";
+        
+        
+        
+    }else{
+        document.getElementById("message").style.color = "red";
+        document.getElementById("message").innerHTML = "password not match";
+        
+         
+    };
+};
